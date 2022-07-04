3646faa4376226ad16cc964d2a273c425b81f83f
hsol19 <kimhgst919@naver.com>
2022-07-05 00:50:20 +0900

    220705_1

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -11355,7 +11355,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4161" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1114" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
